de94c832b47d45be1da3e738c851886a00adecd5
Lanture1064 <34346740+Lanture1064@users.noreply.github.com>
2018-10-22 20:19:32 +0800

    181022.2019 Onclass Update

--- 考试报名系统.cpp
+++ 考试报名系统.cpp
@@ -2,42 +2,35 @@
 #include <string>
 using namespace std;
 
-typedef struct Lnode
+struct sData
 {
-	int i_StudentTestID;
-	string s_StudentName;
-	bool b_StudentGender;
-	int i_StudentAge;
-	string s_ApplyIntention;
-	struct Lnode *next;
-}NODE;
-
-class LinkList
-{
-private:
-	NODE *head;
-public:
-	LinkList(){head = nullptr;}
-	~LinkList();
-	bool clearList();
-	bool IsEmpty(){return head == nullptr;}
-	int ListLength();
-	bool Insert();
-	bool Erase();
-	bool Search(string name);
-	
+	string studentName;
+	string studentAge;
+	string studentGender;
+	string studentID;
+	string applyIntention;
 };
 
-linkList::linkList()
+struct chainNode
 {
-	cout <<"正在建立一位同学的档案" << endl;
-}
+	sData studentData; 
+	chainNode *next = nullptr;
 
-bool linklist::clearList()
+	chainNode(){}
+	chainNode(sData Input)
+	    {this->studentData = Input}
+	chainNode(sData Input,chainNode *next)
+	    {this->studentData = Input; this->next = next}
+};
+
+class chain
 {
+public:
+	chain(int initialCapacity = 10);
+	chain(const chain&);
+	~chain();
 
-}
+	bool empty()const{return listSize == 0;}
+	int size() const {return listSize;}
 
-linkList::~linkList()
-{
 }
